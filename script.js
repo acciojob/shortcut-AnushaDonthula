ca7faa504b94a8fedc01a6f251c0c87ed1a3dc37
AnushaDonthula <162703733+AnushaDonthula@users.noreply.github.com>
2024-07-29 23:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+  // Check if either of the strings is empty
+  if (str1 === "" || str2 === "") {
+    return ""; // Return an empty string if any input string is empty
+  }
+
+  // Return the first letter of each string concatenated
+  return str1[0] + str2[0];
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// Test cases
+console.log(shortcut("Amnesty", "International")); // 'AI'
+console.log(shortcut("Hello", "world"));           // 'Hw'
+console.log(shortcut("", "International"));        // ''
+console.log(shortcut("Amnesty", ""));
